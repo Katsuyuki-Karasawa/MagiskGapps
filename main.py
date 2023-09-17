49dfd8c5a494559d241a4f68454b66359c111a7d
R0w9h <4ranci0ne@gmail.com>
2023-09-18 00:10:33 +0900

    fix main.py

--- main.py
+++ main.py
@@ -1,84 +1,99 @@
 import os
 import json
-from pyfastcopy import copytree, copy2
+import shutil
 import zipfile
 import fnmatch
-import shutil
 
+with open("module-info.json") as f:
+    contents = json.load(f)
+id = contents["result"]["id"]
+name = contents["result"]["name"]
+version = contents["result"]["version"]
+versionCode = contents["result"]["versionCode"]
+author = contents["result"]["author"]
+description = contents["result"]["description"]
 
-def load_module_info():
-    with open("module-info.json") as f:
-        contents = json.load(f)
-    module_info = contents["result"]
-    return module_info
-
-
-def create_module_prop_file(module_info):
-    with open("template/module.prop", "w") as f:
-        f.write(
-            f'id={module_info["id"]}\nname={module_info["name"]}\nversion={module_info["version"]}\nversionCode={module_info["versionCode"]}\nauthor={module_info["author"]}\ndescription={module_info["description"]}'
-        )
-
-
-def extract_and_move_files():
-    with zipfile.ZipFile("gapps.zip", "r") as zip_ref:
-        zip_ref.extractall("gapps")
-
-    os.mkdir("AppSet")
-
-    rootPath = r"gapps/AppSet"
-    pattern = "*.zip"
-
-    for root, dirs, files in os.walk(rootPath):
-        for filename in fnmatch.filter(files, pattern):
-            print("Moving " + os.path.join(root, filename))
-            with zipfile.ZipFile(os.path.join(root, filename)) as zip_ref:
-                zip_ref.extractall(os.path.join("appset/"))
-            os.remove("appset/installer.sh")
-
-    os.remove("appset/uninstaller.sh")
-    print("Moving files")
-
-
-def rename_files():
-    path = "appset"
-    os.chmod(path, 0o777)
-    filenames = os.listdir(path)
-
-    for filename in filenames:
-        src_file = os.path.join(path, filename)
-        dst_file = "appset/" + filename.replace("___", "/")
-        print(f"Renaming {src_file} to {dst_file}")
-        os.rename(src_file, dst_file)
-
-
-def build_module(module_info):
-    source_folder = r"template"
-    destination_folder = r"builds"
-    copytree(source_folder, destination_folder)
-
-    source_folder = r"appset"
-    destination_folder = r"builds/system"
-    copytree(source_folder, destination_folder)
-    print("Building Module")
-
-    shutil.make_archive(
-        f"releases/MagiskGApps-{module_info['version']}", "zip", "builds"
+# Create module.prop
+with open("template/module.prop", "w") as f:
+    f.write(
+        "id="
+        + id
+        + "\n"
+        + "name="
+        + name
+        + "\n"
+        + "version="
+        + version
+        + "\n"
+        + "versionCode="
+        + versionCode
+        + "\n"
+        + "author="
+        + author
+        + "\n"
+        + "description="
+        + description
     )
-    print("Building Zip and archiving")
+
+# Unzip and move AppSet files
+with zipfile.ZipFile(os.path.join(".", "gapps.zip"), "r") as zip_ref:
+    zip_ref.extractall("gapps")
+
+# Define the appsetPath variable to point to the 'appset' directory
+appsetPath = os.path.join(".", "appset")
+
+# Check if the 'appset' directory exists before proceeding
+if not os.path.exists(appsetPath):
+    os.makedirs(appsetPath)
+
+rootPath = os.path.join("gapps", "AppSet")
+pattern = "*.zip"
+for root, dirs, files in os.walk(rootPath):
+    for filename in fnmatch.filter(files, pattern):
+        print("Moving " + os.path.join(root, filename))
+        zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join("appset"))
+        os.remove("appset/installer.sh")
+        os.remove("appset/uninstaller.sh")
+print("Moving files")
+
+# Renames Files from ___ to /
+path = "appset"
+os.chmod(path, 0o777)
+for filename in os.listdir(appsetPath):
+    src_file = os.path.join(appsetPath, filename)
+    dst_file = os.path.join(appsetPath, filename.replace("___", os.sep).lstrip(os.sep))
+
+    # Check if the destination directory exists, if not create it
+    dst_dir = os.path.dirname(dst_file)
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir)
+
+    if os.path.exists(dst_file):
+        if os.path.isdir(dst_file):
+            shutil.rmtree(dst_file)
+        else:
+            os.remove(dst_file)
+
+    shutil.move(src_file, dst_file)
 
 
-def clean_up():
-    shutil.rmtree("gapps", ignore_errors=True)
-    shutil.rmtree("builds", ignore_errors=True)
-    os.remove("template/module.prop")
-    shutil.rmtree("AppSet", ignore_errors=True)
+# Combines everything
+source_folder = r"template"
+destination_folder = r"builds"
+shutil.copytree(source_folder, destination_folder)
 
+source_folder = r"appset"
+destination_folder = r"builds/system"
+shutil.copytree(source_folder, destination_folder)
+print("Building Module")
 
-# Main script execution
-module_info = load_module_info()
-create_module_prop_file(module_info)
-extract_and_move_files()
-rename_files()
-build_module(module_info)
-clean_up()
+shutil.make_archive("releases/MagiskGApps-" + version, "zip", "builds")
+print("Building Zip and archiving")
+os.chmod("gapps", 0o777)
+os.chmod("builds", 0o777)
+os.chmod("AppSet", 0o777)
+
+shutil.rmtree("gapps", ignore_errors=True)
+shutil.rmtree("builds", ignore_errors=True)
+os.remove(os.path.join("template", "module.prop"))
+shutil.rmtree("AppSet", ignore_errors=True)
